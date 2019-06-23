d8c5b0c1960fba270ec9a2d407a81e1e9b03a117
smileyKJ <kj.invincible@gmail.com>
2019-06-23 12:09:45 +0530

    bug fix

--- portfolio/config/website.js
+++ portfolio/config/website.js
@@ -7,11 +7,17 @@ module.exports = {
   siteTitleAlt: "Jayanth", // Alternative Site title for SEO
   siteTitleShort: "Jayanth", // short_name for manifest
   siteHeadline: "Creating marvelous art & blazginly fast websites", // Headline for schema.org JSONLD
-  siteUrl: "", // Domain of your site. No trailing slash!
+  siteUrl: "http://anybodycancode.dev", // Domain of your site. No trailing slash!
   siteLanguage: "en", // Language Tag on <html> element
-  siteLogo: "", // Used for SEO and manifest
-  siteDescription: "Portfolio website",
-  author: "", // Author for schema.org JSONLD
+  siteLogo: "/logo.png", // Used for SEO and manifest
+  siteDescription: "Playful & Colorful One-Page website with Parallax effect",
+  author: "LekoArts", // Author for schema.org JSONLD
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: "@cara", // Twitter Username
+  ogSiteName: "cara", // Facebook Site Name
+  ogLanguage: "en_US", // Facebook Language
+  googleAnalyticsID: "UA-47519312-5",
 
   // Manifest and Progress color
   themeColor: tailwind.colors.orange,
