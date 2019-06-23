6b94bc1710fc05056fa243f7cda9e9149b0e0f2e
smileyKJ <kj.invincible@gmail.com>
2019-06-23 12:37:05 +0530

    bug

--- portfolio/config/website.js
+++ portfolio/config/website.js
@@ -3,11 +3,11 @@ const tailwind = require("../tailwind");
 module.exports = {
   pathPrefix: "/", // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: "Jayanth Portfolio", // Navigation and Site Title
-  siteTitleAlt: "Jayanth", // Alternative Site title for SEO
-  siteTitleShort: "Jayanth", // short_name for manifest
+  siteTitle: "Cara - Gatsby Starter Portfolio", // Navigation and Site Title
+  siteTitleAlt: "Cara", // Alternative Site title for SEO
+  siteTitleShort: "Cara", // short_name for manifest
   siteHeadline: "Creating marvelous art & blazginly fast websites", // Headline for schema.org JSONLD
-  siteUrl: "http://anybodycancode.dev", // Domain of your site. No trailing slash!
+  siteUrl: "https://cara.lekoarts.de", // Domain of your site. No trailing slash!
   siteLanguage: "en", // Language Tag on <html> element
   siteLogo: "/logo.png", // Used for SEO and manifest
   siteDescription: "Playful & Colorful One-Page website with Parallax effect",
@@ -17,7 +17,6 @@ module.exports = {
   userTwitter: "@cara", // Twitter Username
   ogSiteName: "cara", // Facebook Site Name
   ogLanguage: "en_US", // Facebook Language
-  googleAnalyticsID: "UA-47519312-5",
 
   // Manifest and Progress color
   themeColor: tailwind.colors.orange,
